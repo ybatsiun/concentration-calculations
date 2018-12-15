82fa4acd8464b0d64246bc90ae3d2103b08985f8
Yevhen <ybatsiun@gmail.com>
2018-12-15 14:55:00 +0200

    variate speed constans

--- variate.py
+++ variate.py
@@ -1,15 +1,42 @@
 from config import *
 
-result = []
-# 1st lap
-for key in constants[0]:
-        value = constants[0][key]
-        for key1 in constants[1]:
-                value1 = constants[1][key1]
-                # check length
-                result.append([value,value1])
+# result = []
+
+# for key in constants[0]:
+#     value = constants[0][key]
+#     for key1 in constants[1]:
+#         value1 = constants[1][key1]
+#         # check length
+#         result.append([value, value1])
+
+# print("FIRST")
+# print(result)
 
 
-print(result)
+def globalFunc(constants):
+        def work(index,arr):
+                for key in constants[index]:
+                        #breakpoint()
+                        value = constants[index][key]
+                        arr.append(value)
+                        if(index+1 >= constantsLength):
+                                #breakpoint()
+                                result.append(arr.copy())
+                                arr.pop()
+                        else:
+                                #breakpoint()
+                                work(index+1,arr)
+                                arr.pop()
 
+        result = []
+        constantsLength = len(constants)
 
+        work(0,[])
+        return result
+
+    
+
+s = globalFunc(constants)
+print("SECOND")
+print(s)
+print(len(s))
