dc8bf8f87ccc1e98d9238c80c33b6d79dc6fb7e7
Yevhen <ybatsiun@gmail.com>
2019-03-26 01:02:59 +0200

    time interval division

--- calculateService.py
+++ calculateService.py
@@ -4,34 +4,32 @@ import matplotlib.pyplot as plt
 from config import *
 
 
-def calculateConcentrationsLines(systemObj, constants):
+def calculateConcentrationsLines(systemObj, constants, timeInterval):
     initialConcentrations = []
     for reagentName in systemObj['reagentsList']:
         configVal = calculation['INITIAL_CONCENTRATIONS'][reagentName]
         initialConcentrations.append(configVal)
 
-    tAxis = np.linspace(calculation['TIME_INTERVAL'][0],
-                        calculation['TIME_INTERVAL'][1], calculation['INTEGRATION_INTERVAL'])
+    tAxis = np.linspace(timeInterval[0],
+                        timeInterval[1], calculation['INTEGRATION_INTERVAL'])
 
     def pend(y, tAxis, concentrationsSigns, equations, constants):
 
         # assign C_A,C_B ... C_n to values from initial concentrations array
-        for i in range(0,len(concentrationsSigns)):
+        for i in range(0, len(concentrationsSigns)):
             globals()[concentrationsSigns[i]] = y[i]
 
         # assign k1,k2...kn to values from constants array
         for i in range(0, len(constants)):
             globals()['k'+str(i+1)] = constants[i]
 
-        k = [eval(equations[0]), eval(equations[1]),eval(equations[2]), eval(equations[3])]
-        return [eval(equations[0]), eval(equations[1]),eval(equations[2]), eval(equations[3])]
+        return [eval(equations[0]), eval(equations[1]), eval(equations[2]), eval(equations[3])]
 
     concentrationsSigns = systemObj['concentrationsSigns']
     equations = systemObj['system']
     sol = odeint(pend, initialConcentrations, tAxis, args=(
         concentrationsSigns, equations, constants))
 
-        
     return sol
 
 
@@ -41,9 +39,14 @@ def getCalculationsSetByVariants(systemObj, constantsSet):
     for constantsSet in constantsSet:
         obj = {}
         obj['constantsSet'] = constantsSet
-        obj['concentrationsLine'] = calculateConcentrationsLines(
-            systemObj, constantsSet)
-        calculateConcentrationsLines(systemObj, constantsSet)
+        obj['data'] = []
+        for timeValue in range(calculation['TIME_INTERVAL'][0], calculation['TIME_INTERVAL'][1], calculation['STEP_TO_DIVIDE']):
+            subTimeIntervalObj = {}
+            subTimeIntervalObj['timeInterval'] = [
+                timeValue, timeValue+calculation['STEP_TO_DIVIDE']]
+            subTimeIntervalObj['concentrationsVsTime'] = calculateConcentrationsLines(systemObj,constantsSet,subTimeIntervalObj['timeInterval'])
+            obj['data'].append(subTimeIntervalObj.copy())
+
         result.append(obj)
 
     return result
--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 constants = [
     {
         'min': 1,
-        'max': 3,
-        'step':1
+        'max': 30,
+        'step': 5
     },
     {
         'min': 4,
@@ -19,10 +19,14 @@ constants = [
 calculation = {
     'equations': ["3A=2B", "B+2C->D"],
     'INTEGRATION_INTERVAL': 1000,
-    'TIME_INTERVAL': [0, 5],
+    'TIME_INTERVAL': [0, 8],
+    'STEP_TO_DIVIDE': 4,
     'INITIAL_CONCENTRATIONS': {"A": 6, "B": 0, "C": 8, "D": 0}
 }
 
 # TODO
 # amount of constants != to constants in equations
 # amount initial concentrations != reagents
+
+# what constans is to what equation?
+# write ready objects to .json to prevent big data being kept in a memory
