a1a105307dd5b8763646542d5201c23f6305f2bb
Yevhen <ybatsiun@gmail.com>
2019-04-18 21:53:03 +0300

    parsing draft is done

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 import re
 from string import Template
 
+
 def testEval(s):
-    print('evaluating...')
+    print("evaluating...")
     eval(s)
+
 
 # def f():
 #     return globals()["a"] * globals()["b"]
@@ -13,36 +15,37 @@ def testEval(s):
 # a = 2
 # print(lol)
 
-#teststing = ['globals()["a"]','*','globals()["b"]']
+# teststing = ['globals()["a"]','*','globals()["b"]']
 
 # def generate(teststing):
 #    def subf():
 #      for w in teststing:
 
-
-
-string = "2*k1*C_A**3 + 2*k2*C_B**2 - 1*k3*C_B*C_C**2"
-string = '2*k1*C_A**3'
+k1=1
+C_A=1
+#string = "2*k1*C_A**3 + 2*k2*C_B**2 - 1*k3*C_B*C_C**2"
+string = '2*k1*C_A**3 + 3'
 nstring = string.replace("**", "^")
-splitted = re.split("(\-|\+|\*|\n)", nstring)
+splitted = re.split("(\-|\+|\*|\^|\n)", nstring)
 newSplitted = []
 for val in splitted:
     newSplitted.append(val.strip().replace("^", "**"))
 
 varKeys = ["C_A", "C_B", "C_C", "k1", "k2"]
-
 finalSplitted = []
 for val in newSplitted:
 
     index = [i for i, item in enumerate(varKeys) if item in val]
     if len(index) != 0:
-      finalSplitted.append('globals()["' + val + '"]')
+        finalSplitted.append('globals()["' + val + '"]')
     else:
-      finalSplitted.append(val)
+        finalSplitted.append(val)
 
-print(finalSplitted)
+template = "def func(): return "
 
+for s in finalSplitted:
+    template = template + s
 
-# fruit_list = ['raspberry', 'apple', 'strawberry']
-# berry_idx = [i for i, item in enumerate(fruit_list) if 'ras' in item]
-# print(berry_idx)
+print(template)
+exec(template)
+print(func())
