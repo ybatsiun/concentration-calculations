8c5d4d50b4133cf8ce19ac538d88c7d37ae12110
Yevhen <ybatsiun@gmail.com>
2019-04-21 14:43:23 +0300

    fixed writing to json, sorting of equation data, minor fixes-2

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import services.parsingService
 import services.mlService
 from config import *
 import time
+import numpy as np
 
 # TODO move it to another file to make a separate task
 systemObj = services.parsingService.parseEquations(CALCULATION_CONFIG['equations'])
@@ -14,8 +15,27 @@ start = time.time()
 results = services.calculateService.getCalculationsSetByVariants(systemObj,constantsVariations)
 print(time.time() - start)
 networks = services.mlService.getTrainNetworks(results)
-# predict
-experimentalData = fsService.readJsonFile('input.json')
+#experimentalData = fsService.readJsonFile('input.json')
+experimentalData1 = fsService.readJsonFile('results.1.json')['0.5x16_2/35/110_corrupted']
+experimentalData2 = fsService.readJsonFile('results.1.json')['5.10x16_2/35/110']
+experimentalData3 = fsService.readJsonFile('results.1.json')['10.15x16_4x35x210']
 
-#services.fsService.writeJsonToFile(results,'results.json')
-#print(results)
+# convert to 2d
+dataset_size = len([experimentalData1])
+experimentalData_2d_1 = np.asarray(experimentalData1).reshape(dataset_size,-1)
+experimentalData_2d_2 = np.asarray(experimentalData2).reshape(dataset_size,-1)
+experimentalData_2d_3 = np.asarray(experimentalData3).reshape(dataset_size,-1)
+
+pr = networks[0].predict(experimentalData_2d_1)
+print('prediction1')
+print(pr)
+
+pr = networks[0].predict(experimentalData_2d_2)
+print('prediction2')
+print(pr)
+
+pr = networks[0].predict(experimentalData_2d_3)
+print('prediction3')
+print(pr)
+
+fsService.writeJsonToFile(results,'results.json')
