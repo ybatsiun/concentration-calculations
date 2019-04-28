d01b57d6001eb287e3ef93ff2d24dc2ea8da6ddc
Yevhen <ybatsiun@gmail.com>
2019-04-28 14:41:34 +0300

    disabled writing to file

--- server/machineLearningModule/main.py
+++ server/machineLearningModule/main.py
@@ -32,5 +32,5 @@ predictions = mlService.getPredictionsArray(
 
 metricsService.getRelativeError(predictions)
 
-fsService.writeJsonToFile(results, 'server/machineLearningModule/results.json')
+#fsService.writeJsonToFile(results, 'server/machineLearningModule/results.json')
 log('process was ended successfully')
