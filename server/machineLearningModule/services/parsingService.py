35ec5ac9f09221deeaeea33b4b8c343cce4ca619
Yevhen <ybatsiun@gmail.com>
2019-05-07 08:27:08 +0300

    html representation of equations

--- server/machineLearningModule/services/parsingService.py
+++ server/machineLearningModule/services/parsingService.py
@@ -63,16 +63,24 @@ def getEquations(rea):
         i += 1
 
     equa = []  # список со слагаемыми уравнений
+    equa_html = []
     concentrationsSigns = []
     i = 0
     for s in rea2:  # собираем эти слагаемые
         k = "k" + str((i + 1)) + "*"
+        k_html = 'k<sub>' + str((i + 1)) +'</sub>'
         for t in s[0]:
             if t[0] == "1":
+                # the one for python operations
                 k = k + "C_{}*".format(t[1])
-
+                # the one for html represantation
+                k_html = k_html + 'C<sub>{}</sub>'.format(t[1])
             else:
+                # the one for python operations
                 k = k + "C_{}**{}".format(t[1], t[0])
+                # the one for html represantation
+                k_html = k_html + 'C<sub>{}</sub><sup>{}</sup>'.format(t[1], t[0])
+
 
         # remove the last '*' if it exists
         kf = k.rfind("*")
@@ -81,14 +89,16 @@ def getEquations(rea):
             equa.append(new_string)
         else:
             equa.append(k)
-
+        equa_html.append(k_html)
         i += 1
+
     equations = []
     reagentsList = []
     equationDataArray = []
     for s in agg:  # собираем конечные уравнения
         equationData = {}
-        k = "dC({})/dt = ".format(s)
+        k = "dC_{}/dt = ".format(s)
+        k_html = '   <p>dC<sub>{}</sub>/dt = '.format(s)
         equationData['reagent'] = s
         i = -1
         flag = True
@@ -114,13 +124,16 @@ def getEquations(rea):
                                     "1" if n == "" else n,
                                     equa[i],
                                 )
+                                k_html = k_html + '{}{}{}'.format(l if l == '-' else '', n, equa_html[i])
                                 flag = False
                             else:
                                 k = k + " {} {}*{}".format(
                                     l, "1" if n == "" else n, equa[i]
                                 )
+                                k_html = k_html + ' {} {}{}'.format(l, n, equa_html[i])
 
         equationData['equation'] = k
+        equationData['html'] = k_html
         equationDataArray.append(equationData)
 
     return equationDataArray
